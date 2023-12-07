8556b1f1b724a4981d74e7e7d03d4da872d7fa77
punpnx <87162439+punpnx@users.noreply.github.com>
2023-12-08 01:13:37 +0700

    Add files via upload

---
+++ ex1/ข้อ2/Ellipse.java
@@ -0,0 +1,6 @@
+public class Ellipse extends FootShape {
+    @Override
+    public void draw() {
+        System.out.println("draw ellipse");
+    }
+}
---
+++ ex1/ข้อ2/Rectangle.java
@@ -0,0 +1,6 @@
+public class Rectangle extends FootShape {
+    @Override
+    public void draw() {
+        System.out.println("draw rectangle");
+    }
+}
